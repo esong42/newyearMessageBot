6968b40bb8ce56c9831a36defb9800bc95358528
doyoungkim-code <ehehkwat1@naver.com>
2024-01-11 03:38:11 +0900

    전체적인 스타일 수정 및 js flex 기능 부분 변경, 다시하기 버튼 추가

--- test/index.js
+++ test/index.js
@@ -1,25 +1,56 @@
-const containerIntro = document.getElementById('container-intro');
-const containerName = document.getElementById('container-name');
-const container2023 = document.getElementById('container-2023');
-const container2024 = document.getElementById('container-2024');
-const containerMessage = document.getElementById('container-message');
+
+function showContainer(containerId) {
+    // 모든 container를 숨김
+    document.querySelectorAll('.container').forEach(function(container) {
+        container.style.display = 'none';
+    });
+
+    // 특정 container만 보이도록 설정
+    document.getElementById(containerId).style.display = 'flex';
+}
 
 function moveName() {
-    containerIntro.style.display = 'none';
-    containerName.style.display = 'block';
+    showContainer('container-name');
 }
 
 function move2023() {
-    containerName.style.display = 'none';
-    container2023.style.display = 'block';
+    showContainer('container-2023');
 }
 
-function move2024() {        
-    container2023.style.display = 'none';
-    container2024.style.display = 'block';
+function move2024() {
+    showContainer('container-2024');
 }
 
 function moveMessageCard() {
-    container2024.style.display = 'none';
-    containerMessage.style.display = 'block';
+    showContainer('container-message');
 }
+
+function CreateNewCard() {
+    showContainer('container-intro');
+}
+
+// const containerIntro = document.getElementById('container-intro');
+// const containerName = document.getElementById('container-name');
+// const container2023 = document.getElementById('container-2023');
+// const container2024 = document.getElementById('container-2024');
+// const containerMessage = document.getElementById('container-message');
+
+// function moveName() {
+//     containerIntro.style.display = 'none';
+//     containerName.style.display = 'block';
+// }
+
+// function move2023() {
+//     containerName.style.display = 'none';
+//     container2023.style.display = 'block';
+// }
+
+// function move2024() {        
+//     container2023.style.display = 'none';
+//     container2024.style.display = 'block';
+// }
+
+// function moveMessageCard() {
+//     container2024.style.display = 'none';
+//     containerMessage.style.display = 'block';
+// }
