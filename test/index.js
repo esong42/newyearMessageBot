5601406ed64589fc66c53b9cde6c040eb952b3bb
pro <hasong1456@gmail.com>
2024-01-10 20:07:48 +0900

    container 순서만 바꿨습니다. 이름을 앞으로 2023,2024를 뒤로.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,16 @@
 const containerIntro = document.getElementById('container-intro');
+const containerName = document.getElementById('container-name');
 const container2023 = document.getElementById('container-2023');
 const container2024 = document.getElementById('container-2024');
-const containerName = document.getElementById('container-name');
 const containerMessage = document.getElementById('container-message');
 
-function move2023() {
+function moveName() {
     containerIntro.style.display = 'none';
+    containerName.style.display = 'block';
+}
+
+function move2023() {
+    containerName.style.display = 'none';
     container2023.style.display = 'block';
 }
 
@@ -14,12 +19,7 @@ function move2024() {
     container2024.style.display = 'block';
 }
 
-function moveName() {
-    container2024.style.display = 'none';
-    containerName.style.display = 'block';
-}
-
 function moveMessageCard() {
-    containerName.style.display = 'none';
+    container2024.style.display = 'none';
     containerMessage.style.display = 'block';
 }
